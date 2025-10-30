bac7259b554a7baebe4fc30f314bcb69b58dfbb3
AlessandroSanchi <alessandro.sanchi52@studenti.isissgobetti.it>
2025-10-30 12:46:23 +0100

    aggiunte classi

---
+++ auto.py
@@ -0,0 +1,7 @@
+from veicolo.py import veicolo
+class auto(veicolo):
+    def __init__(self,marca,modello):
+        self.marca=marca
+        self.modello=modello
+
+    
---
+++ veicolo.py
@@ -0,0 +1,4 @@
+class veicolo():
+    def __init__(self,ruote,portiere):
+        self.ruote = ruote
+        self.portiere = portiere
